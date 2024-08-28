a687909b6c244ae436efad9a91b3a21571720af8
Jewel-190237 <190237@ku.ac.bd>
2024-08-28 11:51:16 +0600

    initial setup added

---
+++ src/components/Shared/Navbar.jsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { IoBagOutline } from "react-icons/io5";
+
+
+
+const Navbar = () => {
+
+    const navLinks = <>
+        <li> <Link href='/'>Home </Link> </li>
+        <li> <Link href='/about'>About </Link> </li>
+        <li> <Link href='/services'>Services</Link> </li>
+        <li> <Link href='/blog'>Blog</Link> </li>
+        <li> <Link href='/content'>Content</Link> </li>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {
+                                navLinks
+                            }
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">
+                        <Image alt="Logo" src="/assets/logo.svg" width="50" height="80"></Image>
+                    </a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {
+                            navLinks
+                        }
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <div className="flex gap-3 text-2xl">
+                        <IoIosSearch></IoIosSearch>
+                        <IoBagOutline></IoBagOutline>
+                    </div>
+                    <a className="btn btn-outline btn-secondary text-white ml-2">Appointment</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
