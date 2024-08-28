ea8ab6066a475c10ae76f73f42aec7c23a6a573e
Jewel-190237 <jewel190237@gmail.com>
2024-08-29 00:52:31 +0600

    file uploaded

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -1,24 +1,56 @@
+import Image from "next/image";
 import React from "react";
 
 const Banner = () => {
 
+    const banners = [
+        {
+            title: 'Affordable Price For Car Servicing',
+            description: 'There are many variations of passages of  available, but the majority have suffered alteration in some form',
+            next: '#slide2',
+            prev: '#slide4'
+        },
+        {
+            title: 'Affordable Price For Car Servicing',
+            description: 'There are many variations of passages of  available, but the majority have suffered alteration in some form',
+            next: '#slide3',
+            prev: '#slide1'
+        },
+        {
+            title: 'Affordable Price For Car Servicing',
+            description: 'There are many variations of passages of  available, but the majority have suffered alteration in some form',
+            next: '#slide4',
+            prev: '#slide2'
+        },
+        {
+            title: 'Affordable Price For Car Servicing',
+            description: 'There are many variations of passages of  available, but the majority have suffered alteration in some form',
+            next: '#slide1',
+            prev: '#slide3'
+        },
+
+    ]
 
     return (
         <div className="carousel w-full">
-            <div id="slide1" className="carousel-item relative w-full">
-                <div
-                    style={{
-                       backgroundImage: 'lini' 
-                    }}
-                >
+            {
+                banners.map((banner, index) => (
+                    <div
+                        style={{ backgroundImage: `url('/assets/images/banner/${index+1}.jpg')` }}
+                        key={index}
+                        id={`slide${index+1}`}
+                        className="carousel-item relative w-full h-screen"
+                    >
+                        <div className="">
 
-                    <h1>Lorem ipsum dolor sit amet.</h1>
-                </div>
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
-                </div>
-            </div>
+                        </div>
+                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                            <a href={banner.prev} className="btn btn-circle">❮</a>
+                            <a href={banner.next} className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
