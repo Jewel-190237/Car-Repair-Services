f7e1f1315efec7e1aa2a0eaae1c3b50fd027fa85
Jewel-190237 <190237@ku.ac.bd>
2024-08-29 15:29:37 +0600

    file uploaded

--- src/Services/AuthProvider.jsx
+++ src/Services/AuthProvider.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
     return (
         <div>
-            
+            {
+                children
+            }
         </div>
     );
 };
