95e2099a24c31c6530a7a09335dfcd1a3b46432a
Jewel-190237 <jewel190237@gmail.com>
2024-08-29 11:04:20 +0600

    file uploaded

--- src/components/HomePage/About.jsx
+++ src/components/HomePage/About.jsx
@@ -1,10 +1,20 @@
+import Image from 'next/image';
 import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div className='flex flex-col lg:flex-row mt-20 gap-10'>
+                <div className='flex-1'>
+                    <Image className='rounded-lg ' src='/assets/images/about_us/person.jpg' width={460} height={100} alt='person' />
+                    <Image className='rounded-lg relative border-4 border-white bottom-36 left-48' src='/assets/images/about_us/parts.jpg' width={327} height={100} alt='person' />
+                </div>
+                <div className='flex-1'>
+
+                </div>
+
+            </div>
+        </section>
     );
 };
 
