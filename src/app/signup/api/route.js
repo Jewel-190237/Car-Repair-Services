925d67cd659a956f8c2847471496e53963d2e01f
Jewel-190237 <jewel190237@gmail.com>
2024-08-29 23:24:04 +0600

    db added

--- src/app/signup/api/route.js
+++ src/app/signup/api/route.js
@@ -1,9 +1,21 @@
+import { connectDB } from "@/components/lib/connectDB";
+import { message } from "antd";
+import Email from "next-auth/providers/email";
 
 export const POST = async (request) => {
     const newUser = await request.json();
     try{
-        const db = await 
+        const db = await connectDB()
+        const userCollection = await db.collection('users')
+        const res = await userCollection.insertOne(newUser)
+
+        const exists = userCollection.findOne({email: newUser.email})
+        if(exists) {
+            return Response.json({message: 'user already exists'}, {status: 304})
+        }
+
+        return Response.json({message: 'user created'}, {status: 200})
     }catch(error){
-        console.log(error)
+        return Response.json({message: 'something went wrong'}, {status: 500})
     }
 }
