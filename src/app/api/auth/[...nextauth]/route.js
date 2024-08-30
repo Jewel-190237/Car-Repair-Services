32f8c3e58aae4608f81fd5c6e6c704ab1d215289
Jewel-190237 <190237@ku.ac.bd>
2024-08-30 16:11:07 +0600

    file uploaded

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
+import { connectDB } from "@/components/lib/connectDB";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 const handler = NextAuth({
     session: {
@@ -13,7 +15,21 @@ const handler = NextAuth({
                 password: {}
             },
             async authorize(credentials) {
-                return true;
+                const { email, password } = credentials;
+                if (!email || !password) {
+                    return null;
+                }
+
+                const db = connectDB();
+                const currentUser = await db.collection('users').findOne({ email })
+                if (!currentUser) {
+                    return null;
+                }
+                const passwordMatch = bcrypt.compareSync(password, currentUser.password); // true
+                if (!passwordMatch) {
+                    return null;
+                }
+                return currentUser;
             }
         })
     ],
