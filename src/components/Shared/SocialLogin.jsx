a8c3cc52c484221ba67f5afb38fb30bbc8d8f8da
Jewel-190237 <jewel190237@gmail.com>
2024-08-31 14:40:20 +0600

    file uploaded

--- src/components/Shared/SocialLogin.jsx
+++ src/components/Shared/SocialLogin.jsx
@@ -1,14 +1,21 @@
+import { signIn } from 'next-auth/react';
 import React from 'react';
 import { AiFillGoogleCircle } from 'react-icons/ai';
-import { FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const SocialLogin = () => {
+
+    const handleSocialLogin = async(provider) => {
+        const resp = await signIn(provider);
+
+    }
+
     return (
         <div>
             <div className='mt-5 flex text-3xl justify-center text-blue-800 gap-3'>
-                <button> <FaFacebook /> </button>
-                <button><FaLinkedin /> </button>
-                <button><AiFillGoogleCircle /> </button>
+                <button onClick={() => handleSocialLogin('gitHub')}> <FaGithub/> </button>
+                <button onClick={() => handleSocialLogin('google')}><AiFillGoogleCircle /> </button>
+                <button onClick={() => handleSocialLogin('facebook')}> <FaFacebook /> </button>
             </div>
         </div>
     );
