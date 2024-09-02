01f5938cfb0d4adc4124f95571ec04c89286cadb
Jewel-190237 <jewel190237@gmail.com>
2024-09-02 17:25:32 +0600

    file uploaded

--- src/Services/getServices.js
+++ src/Services/getServices.js
@@ -3,3 +3,8 @@ export const getService = async () => {
     const data = res.json();
     return data;
 }
+export const getServiceDetails = async (id) => {
+    const res = await fetch(`http://localhost:3000/services/api/${id}`);
+    const data = res.json();
+    return data;
+}
