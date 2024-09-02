ac7cf5d624568f70d87b4ac309f846e0ea06b5ee
Jewel-190237 <jewel190237@gmail.com>
2024-09-02 12:09:11 +0600

    file uploaded

--- src/components/Shared/ThemeControl.jsx
+++ src/components/Shared/ThemeControl.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 const ThemeControl = () => {
     const [darkMode, setDarkMode] = useState(true);
@@ -12,19 +12,22 @@ const ThemeControl = () => {
     }, [])
 
     useEffect(() => {
-        if (darkMode){
+        if (darkMode) {
             document.documentElement.classList.add('dark')
-            localStorage.setItem("theme","dark")
+            localStorage.setItem("theme", "dark")
         }
-        else{
+        else {
             document.documentElement.classList.remove('dark')
             localStorage.setItem("theme", "light")
         }
     }, [darkMode])
 
     return (
-        <div onClick={() => setDarkMode(!darkMode)} className='relative w-16 h-8 flex items-center dark:bg-red-800 bg-green-950'>
-            <FaMoon className='text-white size-20'/>
+        <div onClick={() => setDarkMode(!darkMode)} className='relative w-16 h-8 flex items-center dark:bg-red-800 bg-green-950 rounded-full p-1 cursor-pointer'>
+            <FaMoon className='text-white size-20' />
+            <div className='absolute bg-white dark:bg-medium w-8 h-8 rounded-full shadow-md transform transition-transform duration-300 ' style={darkMode ? { left: '2px' } : { right: '2px' }}>
+                <FaSun className='ml-auto size-20 text-yellow-400' />
+            </div>
 
         </div>
     );
