f34c01796472a9cf87105790984555c153f832b5
Jewel-190237 <jewel190237@gmail.com>
2024-09-02 18:02:52 +0600

    file uploaded

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,9 +1,13 @@
+import { getServiceDetails } from '@/Services/getServices';
 import React from 'react';
 
-const page = () => {
+const page = async({params}) => {
+    const serviceDetails = await getServiceDetails(params.id);
+    // console.log(serviceDetails);
+    const {img, price, title, description, facility} = serviceDetails.service
     return (
         <div>
-            
+            service details page
         </div>
     );
 };
