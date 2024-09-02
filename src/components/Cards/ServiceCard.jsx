5da4283d6a6320ac9d7733268374e97cb5891f19
Jewel-190237 <jewel190237@gmail.com>
2024-09-02 17:15:03 +0600

    file uploaded

--- src/components/Cards/ServiceCard.jsx
+++ src/components/Cards/ServiceCard.jsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { GoArrowRight } from "react-icons/go";
 
 
 const ServiceCard = ({ service }) => {
 
-    const { title, price, img } = service || {};
+    const { title, price, img, _id } = service || {};
     return (
         <div>
+            
             <div className="card bg-base-100 shadow-xl">
                 <figure>
                     <Image className='rounded-lg' src={img} alt='img' width={350} height={120} />
@@ -16,7 +18,7 @@ const ServiceCard = ({ service }) => {
                     <h2 className="card-title lato text-xl">{title}</h2>
                     <div className="card-actions justify-between items-center">
                         <p className='text-[#FF3811] mt-4 lato font-bold'>Price: ${price}</p>
-                        <button><GoArrowRight className='text-[#FF3811] text-xl font-bold' /></button>
+                        <Link href={`/services/${_id}`}> <GoArrowRight className='text-[#FF3811] text-xl font-bold' /></Link>
                     </div>
                 </div>
             </div>
