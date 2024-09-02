249859c2cf52a7aea69fb4813b58b631f9900642
Jewel-190237 <190237@ku.ac.bd>
2024-09-02 16:02:49 +0600

    file uploaded

--- src/app/services/seed/route.jsx
+++ src/app/services/seed/route.jsx
@@ -1,7 +1,17 @@
 import { connectDB } from "@/lib/connectDB"
+import { services } from "@/lib/services";
 
 export const GET = async () => {
     const db = await connectDB();
-    const serviceCollection = db.collection("services")
-    
+    const serviceCollection = db.collection("services");
+
+    try {
+        await serviceCollection.deleteMany();
+        const resp = serviceCollection.insertMany(services);
+        return Response.json({ message: "services added successfully" })
+
+    } catch (error) {
+        console.log(error)
+    }
+
 }
