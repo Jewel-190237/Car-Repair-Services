22377cf2387bbae3c74976c74be79250f5f08de3
Jewel-190237 <jewel190237@gmail.com>
2024-09-02 11:49:40 +0600

    file uploaded

--- src/components/Shared/ThemeControl.jsx
+++ src/components/Shared/ThemeControl.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FaMoon } from 'react-icons/fa';
 
 const ThemeControl = () => {
+    const [darkMode, setDarkMode] = useState(true);
+
+    useEffect(() => {
+        const theme = localStorage.getItem("theme")
+        if (theme === "dark") {
+            setDarkMode(true);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (darkMode){
+            document.documentElement.classList.add('dark')
+            localStorage.setItem("theme","dark")
+        }
+        else{
+            document.documentElement.classList.remove('dark')
+            localStorage.setItem("theme", "light")
+        }
+    }, [darkMode])
+
     return (
-        <div>
-            
+        <div onClick={() => setDarkMode(!darkMode)} className='relative w-16 h-8 flex items-center dark:bg-red-800 bg-green-950'>
+            <FaMoon className='text-white size-20'/>
+
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "dark" : "#232A3C",
+        "medium": "#293245"
+      }
       
     },
   },
