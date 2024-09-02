a9678b5b998a23cd7bc83de94d4659060902164f
Jewel-190237 <190237@ku.ac.bd>
2024-09-02 17:25:32 +0600

    file uploaded

--- src/app/services/api/[id]/route.js
+++ src/app/services/api/[id]/route.js
@@ -1,11 +1,11 @@
 import { connectDB } from "@/lib/connectDB"
 
-export const GET = async () => {
+export const GET = async (request, {params}) => {
     const db = await connectDB();
     const serviceCollection = db.collection("services");
     try {
-        const services = await serviceCollection.find().toArray()
-        return Response.json(services)
+        const service = await serviceCollection.findOne({_id: params.id})
+        return Response.json(service)
 
     } catch (error) {
         console.log(error)
