2664e7d637e93536292ba548e4aadfe9b9d27143
Jewel-190237 <190237@ku.ac.bd>
2024-09-02 17:15:03 +0600

    file uploaded

---
+++ src/Services/getServices.js
@@ -0,0 +1,5 @@
+export const getService = async () => {
+    const res = await fetch("http://localhost:3000/services/api/getAll");
+    const data = res.json();
+    return data;
+}
--- src/components/HomePage/Service.jsx
+++ src/components/HomePage/Service.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-// import { services } from '../../lib/services'
 import ServiceCard from '../Cards/ServiceCard';
+import { getService } from '@/Services/getServices';
 
-const getService = async () => {
-    const res = await fetch("http://localhost:3000/services/api/getAll");
-    const data = res.json();
-    return data;
-}
+
 
 const Service = async () => {
     const services = await getService();
