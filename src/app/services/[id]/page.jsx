99886105efd72327d7dd7d74cd7a0b966b9357d5
Jewel-190237 <jewel190237@gmail.com>
2024-09-03 12:05:13 +0600

    file uploaded

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,14 +1,19 @@
 import { getServiceDetails } from '@/Services/getServices';
+import Image from 'next/image';
 import React from 'react';
 
 const page = async({params}) => {
     const serviceDetails = await getServiceDetails(params.id);
     // console.log(serviceDetails);
-    const {img, price, title, description, facility} = serviceDetails.service
+    // const {img, price, title, description, facility} = serviceDetails.service
     return (
-        <div>
-            service details page
-        </div>
+        <section>
+            <div className='gradientBackground rounded-lg'>
+                <Image className='' src='/details.png' alt='image' height={300} width={1137}/>
+            </div>
+            <h2 className='relative bottom-44 left-28 lato text-5xl font-bold text-white'>Service Details</h2>
+            <div></div>
+        </section>
     );
 };
 
