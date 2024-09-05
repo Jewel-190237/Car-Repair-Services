856734d2aa37915bd1f38eef550b719277bde919
Jewel-190237 <190237@ku.ac.bd>
2024-09-05 11:43:31 +0600

    file uploaded

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -1,15 +1,27 @@
+'use client'
 import { getServiceDetails } from '@/Services/getServices';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutPage = ({params}) => {
+const CheckoutPage = ({ params }) => {
     const [service, setService] = useState({});
-    const loadService = async() => {
-        const serviceDetails = await getServiceDetails(params.id)
-        setService(serviceDetails)
+    const loadService = async () => {
+        const details = await getServiceDetails(params.id)
+        setService(details.service)
     }
+    console.log(service)
+    // const { _id, img, price, description } = service || {};
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    useEffect(() => {
+        loadService()
+
+    }, [params])
     return (
         <div>
-            
+
         </div>
     );
 };
